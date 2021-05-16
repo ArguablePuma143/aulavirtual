25405f0d02f2db83c6ff28c11cd692f31563a9c1
ArguablePuma143 <javiermarore@gmail.com>
2021-05-16 17:53:30 +0200

    Now you can upload files

--- module003/forms.py
+++ module003/forms.py
@@ -2,6 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextField, TextAreaField, DateField
 import datetime
 from wtforms.validators import Required
+from flask_wtf.file import FileRequired, FileField, FileAllowed
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'docx'}
+def allowed_file(filename):
+    """
+    @params: filename - name to be filtered
+    @returns: A boolean depicting if it is allowed (True) or not (False)
+    """
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 class PostForm(FlaskForm):
     name = TextField("Name of the activity", validators=[Required()])
@@ -9,3 +19,8 @@ class PostForm(FlaskForm):
     deadline = DateField("Fecha límite de entrega", format="%d-%m-%Y %H:%M", default=datetime.datetime.today)
     submit = SubmitField('Submit')
 
+class ActivityUploadForm(FlaskForm):
+    file = FileField("Put your file here", 
+                    validators= [FileRequired(), FileAllowed(ALLOWED_EXTENSIONS, "No se permite esa extensión")], 
+                    description="Entrega tu archivo")
+    submit = SubmitField("Entregar")
--- module003/views.py
+++ module003/views.py
@@ -1,17 +1,33 @@
 from flask import Blueprint, render_template, flash, redirect, url_for
+from flask.globals import request
 from flask_login import login_required, current_user
-from models import get_db, Follow, Course, Activity
+from models import get_app, get_db, Follow, Course, Activity, ActivityUpload
 from module003.forms import *
+import os
+from werkzeug.utils import secure_filename
+
+from flask import send_from_directory
 
 module003 = Blueprint("module003", __name__,static_folder="static",template_folder="templates")
 db = get_db()
+app = get_app()
+
+UPLOAD_FOLDER = 'C:\\Users\\Javi\\Desktop\\Web Servidor\\proyecto\\aulavirtual\\module003\\subidas'
+
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 @module003.route('/', methods=['GET', 'POST'])
 def module003_index():
     courses = None
     coursesCreated = Course.query.filter(Course.user_id == current_user.id).all()
     coursesFollowed = Follow.query.filter(Follow.user_id == current_user.id).all()
-    return render_template('module003_index.html', coursesFollowed=coursesFollowed, coursesCreated=coursesCreated, module="module003")
+    return render_template('module003_index.html',
+                coursesFollowed=coursesFollowed,
+                coursesCreated=coursesCreated,
+                module="module003")
 
 @module003.route('/<coursecode>', methods=['GET', 'POST'])
 def module003_course(coursecode):
@@ -42,5 +58,39 @@ def module003_course(coursecode):
 
 @module003.route('/<coursecode>/<activity_id>', methods=['GET', 'POST'])
 def module003_activity(coursecode, activity_id):
+    form = ActivityUploadForm()
+    if form.validate_on_submit():
+        try:
+            file = request.files['file']
+
+            filename = secure_filename(file.filename)
+            folder = UPLOAD_FOLDER + "\\{user_id}\\{course_code}\\{activity_id}".format(
+                user_id=current_user.id,
+                course_code=coursecode,
+                activity_id=activity_id)
+
+            if not os.path.exists(folder):
+                os.makedirs(folder, exist_ok=True)
+            file.save(os.path.join(folder, filename))
+
+            upload = ActivityUpload(
+                user_id=current_user.id,
+                activity_id = activity_id,
+                content_link = filename)
+
+            db.session.add(upload)
+            db.session.commit()
+        except os.error as e:
+            flash("There was an error uploading your file, sorry")
+            redirect("/")
+
     activity = Activity.query.get(activity_id)
-    return render_template("module003_activity.html", activity=activity)
+    coursesCreated = Course.query.filter(Course.user_id == current_user.id).all()
+    coursesFollowed = Follow.query.filter(Follow.user_id == current_user.id).all()
+
+    return render_template("module003_activity.html", 
+                            activity=activity,
+                            coursesFollowed=coursesFollowed,
+                            coursesCreated=coursesCreated,
+                            form=form,
+                            module="module003")
